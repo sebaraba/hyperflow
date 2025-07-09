e478421b604ac76c0f6c5377781a03c3f58273a7
sebaraba <rabasebastians@gmail.com>
2025-07-09 23:14:51 +0300

    chore: rename home to dashboard

---
+++ src/constants/routes.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+export enum Routes {
+  Dashboard = "/",
+  Swap = "/swap",
+  Liquidity = "/liquidity",
+  Vote = "/vote",
+  Lock = "/lock",
+  Incentivize = "/incentivize",
+  About = "/about",
+}
